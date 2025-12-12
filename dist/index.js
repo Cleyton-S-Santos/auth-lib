d7a0d55a61cb49e945201ac9857555da10b56ee5
Cleyton Sousa <cleytonsousadossantos.2021@gmail.com>
2025-12-12 03:39:58 -0300

    fix: expports e package version

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,2 @@
-export * from "./domain/ports/UserRepository";
-export * from "./domain/ports/PasswordHasher";
-export * from "./domain/ports/TokenManager";
-export * from "./domain/ports/CacheStore";
-export * from "./domain/errors";
-export * from "./application/types";
-export * from "./application/AuthService";
+export { AuthService } from "./application/AuthService";
+export { AuthError } from "./domain/errors";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-export * from "./domain/ports/UserRepository";
-export * from "./domain/ports/PasswordHasher";
-export * from "./domain/ports/TokenManager";
-export * from "./domain/ports/CacheStore";
-export * from "./domain/errors";
-export * from "./application/types";
-export * from "./application/AuthService";
+export { AuthService } from "./application/AuthService";
+export type { RegisterInput, LoginInput, ValidationResult } from "./application/types";
+export type { UserRepository } from "./domain/ports/UserRepository";
+export type { PasswordHasher } from "./domain/ports/PasswordHasher";
+export type { TokenManager, VerifiedToken, TokenClaims } from "./domain/ports/TokenManager";
+export type { CacheStore } from "./domain/ports/CacheStore";
+export { AuthError } from "./domain/errors";
+export type { AuthErrorCode } from "./domain/errors";
